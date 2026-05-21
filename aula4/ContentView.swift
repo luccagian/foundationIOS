3a7d0405370c9d71f3cd0f8364ec1e57927beb0d
Gian Lucca Campanha <126993596+luccagian@users.noreply.github.com>
2026-05-21 15:14:38 -0300

    Implement user fetching and display in ContentView
    Fetch and display GitHub user information with avatar.

--- aula4/ContentView.swift
+++ aula4/ContentView.swift
@@ -1,11 +1,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var user: GitHubUser?
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if let user {
+                AsyncImage(url: UR(string: user.avatarUrl)) { 
+                    img in img
+                    .resizable()
+                    .clipShape(.circle)
+                    .frame(width: 300, height: 300)
+                } placeholder: {
+                    ProgressView()
+                }
+                if let name = user.name {
+                    Text(name)
+                        .font(.title)
+                }
+                Text("@\(user.login)"
+            }
+            .padding()
+            .task {
+                try? await user = GitHubAPI.fetchUser(username: "torvalds")
+            }
+        }
     }
 }
 
+                        
 #Preview {
     ContentView()
 }
